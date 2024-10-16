a01c0f81187dfe3d683bbf70d64ca1bdb792235b
HimanshuVishvkarma <himanshuvishwakarma648@gmail.com>
2024-10-16 17:28:41 +0530

    Prime Or Not

--- numprimeornot.java
+++ numprimeornot.java
@@ -1,18 +1,20 @@
+import java.util.Scanner;
 public class numprimeornot {
     public static boolean PrimeOrNot(int n){
-       
-        for(int i=2; i<=n; i++){
-            if (n%i==0) {
-             return false;
-            }else{
-               return true;
-            }
-            
-        }
- 
-    }
-    public static void main(String[] args) {
-           int n;
-        System.out.println(PrimeOrNot(5));
-        }
+        boolean num = true;
+              if(n<=1) return false;
+              for (int i = 2; i < n; i++) {
+                if (n%i==0) {
+                    num = false;
+                }
+              }
+              return num;
+         }
+   public static void main(String[] args) {
+       System.out.println("Enter Your Number:");
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+      System.out.println(PrimeOrNot(n));
+      
+   }
 }
